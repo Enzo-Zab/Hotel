a03d9786be6239a63341149099b8f85f49a8ed2a
ENZO ZABADAL DE OLIVEIRA <enzo_z_oliveira@estudante.sesisenai.org.br>
2024-08-13 11:58:01 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
 import './App.css'
 
 function App() {
-
   return (
     <>
-    <div className="container">
-      <div className="aling">
-        <h1>LOGO</h1>
-        <h4>Gmail</h4>
-        <input type="email" placeholder='Gmail'/>
-        <h4>Senha</h4>
-        <input type="password" placeholder='Senha'/>
-
+      <div className="container">
+        <div className="align">
+          <h1 className='bigger'>Detalhes do Hóspede</h1>
+          
+          <form className="form">
+            <div>
+              <label htmlFor="name">Nome</label>
+              <input type="text" id="name" placeholder='Nome' />
+            </div>
+            <div>
+              <label htmlFor="phone">Telefone</label>
+              <input type="tel" id="phone" placeholder='Telefone' />
+            </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" placeholder='Email' />
+            </div>
+            <div>
+              <label htmlFor="country">País</label>
+              <input type="text" id="country" placeholder='País' />
+            </div>
+            <div>
+              <label htmlFor="password">Senha</label>
+              <input type="password" id="password" placeholder='Senha' />
+            </div>
+            <button type="submit" className="register-button">Cadastrar</button>
+          </form>
+          
+          <p>Já tem uma conta? <a className='link' href="#">Entrar</a></p>
+        </div>
       </div>
-      <div className="link">
-        <button>ENTRAR</button>
-
-        <p>Não é registrado ainda ? <a className='a'>Cadastrar-se</a></p>
-      </div>
-    </div>
     </>
   )
 }
